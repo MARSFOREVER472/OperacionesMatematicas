beeeabd5a7bd823622626d71a886df87e2207f83
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-05-17 12:02:01 -0400

    Comienzo del proyecto.

---
+++ Mathematic_Aritmetic_C++/Mathematic_Aritmetic_C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    // VAMOS A DECLARAR ALGUNAS VARIABLES MATEMÁTICAS...
+
+    double number = -3.1785;
+    int number2 = 6;
+    double PI = 3.14159;
+
+    // CÓMO OBTENER EL VALOR ABSOLUTO ENTRE ESTOS NÚMEROS...
+
+    printf("El valor absoluto de un number es %g y el de number2 es %g", fabs(number), fabs(number2));
+
+    return 0;
+}
